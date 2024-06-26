9ef3ea1c7096b49edd14d3166a502853331e4b56
Juanjo García <juadrigarciagonzalez@gmail.com>
2024-06-26 15:59:03 +0200

    resfonsive fixes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -111,7 +111,7 @@ const Image = ({photo}) => {
                     <Masonry gutter='16px'>
                     {relatedPhotos?.map(photo => (
                       <div className='relative related-photo' key={photo.id} onClick={() => changeLightbox(photo)}>
-                        <img src={photo.urls.small} alt={photo.alt_description} className='w-100 h-100 rounded-[8px] cursor-pointer' loading='lazy'/>
+                        <img src={photo.urls.small} alt={photo.alt_description} className='w-100 h-100 cursor-pointer' loading='lazy'/>
                         <div className='related-info px-1 absolute bottom-0 w-full h-15 flex justify-between align-middle bg-[linear-gradient(180deg,rgba(0,0,0,0),rgba(0,0,0,.7))] opacity-0 transition-opacity duration-300'>
                           <div className='p-3 flex justify-center items-center gap-3'>
                             <img src={photo.user.profile_image.small} alt={photo.alt_description} className='w-10 h-10 rounded-[50%] inline-block'/>
@@ -131,9 +131,9 @@ const Image = ({photo}) => {
             </div>
           </div>
 
-          <div className='absolute ml-4 md:ml-0 md:right-[50px] top-2 md:top-[10%] w-[10%] inline-flex flex-row md:flex-col justify-start items-center md:items-end gap-2 md:min-w-[15%] md:min-h-[50%]' onClick={handleBackgroundClick} ref={infoLightbox}>
-            <a className='p-3 inline md:flex gap-2 flex-col items-end justify-end' href={mainPhoto.user.links.html} target='_blank'>
-              <span className='text-[hsla(0,0%,100%,.79)] text-sm bree-serif-regular hidden md:inline text-nowrap text-right'>Image upload on Unsplash by @{mainPhoto.user.username}</span>
+          <div className='absolute ml-[10%] md:ml-0 md:right-[50px] top-2 md:top-[10%] w-[10%] inline-flex flex-row md:flex-col justify-start items-center md:items-end gap-2 md:min-w-[15%] md:min-h-[50%]' onClick={handleBackgroundClick} ref={infoLightbox}>
+            <a className='md:px-3 py-3 inline md:flex gap-2 flex-col items-end justify-end' href={mainPhoto.user.links.html} target='_blank'>
+              <span className='text-[hsla(0,0%,100%,.79)] text-sm bree-serif-regular hidden md:inline text-balance text-right'>Image upload on Unsplash by @{mainPhoto.user.username}</span>
               <img src={mainPhoto.user.profile_image.small} alt={mainPhoto.alt_description} className='w-[42px] h-[42px] rounded-[50%] inline-block profile-photo'/>
             </a>
 
